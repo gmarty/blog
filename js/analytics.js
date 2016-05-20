deb16a2150aa77b47a8a8134861351515f24dccf
Guillaume Marty <edo999@gmail.com>
2016-05-20 17:15:54 +0100

    Updates

---
+++ js/analytics.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Learn about privacy and Google Analytics in this post:
+// http://gu.illau.me/posts/privacy-and-google-analytics/
+if (navigator.doNotTrack !== '1') {
+  (function(G,o,O,g,l){G.GoogleAnalyticsObject=O;G[O]||(G[O]=function(){(G[O].q=G[O].q||[]).push(arguments)});G[O].l=Date.now();g=o.createElement('script'),l=o.scripts[0];g.src='//www.google-analytics.com/analytics.js';l.parentNode.insertBefore(g,l)}(this,document,'ga'));
+  ga('create', 'UA-207391-17', {
+    'storage': 'none',
+    'clientId': localStorage.getItem('gaClientId'),
+    'siteSpeedSampleRate': 100
+  });
+  ga(function(tracker) {
+    localStorage.setItem('gaClientId', tracker.get('clientId'));
+  });
+  ga('set', 'anonymizeIp', true);
+  ga('set', 'forceSSL', true);
+  ga('send', 'pageview');
+}
